3c48b0338dbd3186f70922de2813d02c41ef2e7f
b6023626 <b6023626@my.shu.ac.uk>
2017-04-21 18:04:35 +0100

    added some blanc if statments

--- Form1.cs
+++ Form1.cs
@@ -12,17 +12,19 @@ namespace _26923626___assignment2
 {
     public partial class Form1 : Form
     {
+        string word = "";
+        int btn1click = 0;
+        int btn2click = 0;
+        int btn3click = 0;
+        int btn4click = 0;
+        int btn5click = 0;
+        int btn7click = 0;
+        int btn8click = 0;
+        int btn9click = 0;
         public Form1()
         {
             InitializeComponent();
-            int btn1click = 0;
-            int btn2click = 0;
-            int btn3click = 0;
-            int btn4click = 0;
-            int btn5click = 0;
-            int btn7click = 0;
-            int btn8click = 0;
-            int btn9click = 0;
+
         }
 
         private void modebtn_Click(object sender, EventArgs e)
@@ -32,11 +34,25 @@ namespace _26923626___assignment2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (btn1click == 5)
+            {
+                btn1click = 1;
+            }
+            else
+            {
+                btn1click++;
+            }
 
-            if(){
+            if(btn1click == 1){
 
 
-            };
+            }
+          
+
+
+
+
+
         }
 
         private void entbtn_Click(object sender, EventArgs e)

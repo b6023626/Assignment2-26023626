3d494cb0d9c4dae195654fda9ed3161ec7424b58
b6023626 <b6023626@my.shu.ac.uk>
2017-04-21 15:06:58 +0100

    FinishedUI with named buttons needs colouring

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,24 +29,232 @@
         private void InitializeComponent()
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.settingbtn = new System.Windows.Forms.Button();
+            this.modebtn = new System.Windows.Forms.Button();
+            this.nextbtn = new System.Windows.Forms.Button();
+            this.prevbtn = new System.Windows.Forms.Button();
+            this.sendbtn = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.symbtn = new System.Windows.Forms.Button();
+            this.triplebtn = new System.Windows.Forms.Button();
+            this.singlebtn = new System.Windows.Forms.Button();
+            this.entbtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(105, 70);
+            this.richTextBox1.Location = new System.Drawing.Point(56, 84);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 81);
+            this.richTextBox1.Size = new System.Drawing.Size(110, 27);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 3);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(260, 75);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // settingbtn
+            // 
+            this.settingbtn.Location = new System.Drawing.Point(173, 85);
+            this.settingbtn.Name = "settingbtn";
+            this.settingbtn.Size = new System.Drawing.Size(75, 23);
+            this.settingbtn.TabIndex = 2;
+            this.settingbtn.Text = "MultiPress";
+            this.settingbtn.UseVisualStyleBackColor = true;
+            // 
+            // modebtn
+            // 
+            this.modebtn.Location = new System.Drawing.Point(12, 117);
+            this.modebtn.Name = "modebtn";
+            this.modebtn.Size = new System.Drawing.Size(52, 31);
+            this.modebtn.TabIndex = 3;
+            this.modebtn.Text = "Mode";
+            this.modebtn.UseVisualStyleBackColor = true;
+            // 
+            // nextbtn
+            // 
+            this.nextbtn.Location = new System.Drawing.Point(71, 117);
+            this.nextbtn.Name = "nextbtn";
+            this.nextbtn.Size = new System.Drawing.Size(52, 31);
+            this.nextbtn.TabIndex = 4;
+            this.nextbtn.Text = "Next";
+            this.nextbtn.UseVisualStyleBackColor = true;
+            // 
+            // prevbtn
+            // 
+            this.prevbtn.Location = new System.Drawing.Point(129, 117);
+            this.prevbtn.Name = "prevbtn";
+            this.prevbtn.Size = new System.Drawing.Size(60, 31);
+            this.prevbtn.TabIndex = 5;
+            this.prevbtn.Text = "Previous";
+            this.prevbtn.UseVisualStyleBackColor = true;
+            // 
+            // sendbtn
+            // 
+            this.sendbtn.Location = new System.Drawing.Point(195, 112);
+            this.sendbtn.Name = "sendbtn";
+            this.sendbtn.Size = new System.Drawing.Size(60, 40);
+            this.sendbtn.TabIndex = 6;
+            this.sendbtn.Text = "Send Predicted";
+            this.sendbtn.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 154);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(52, 40);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7\r\n.,\"";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(70, 154);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(52, 40);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "8\r\nABC";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(129, 154);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(52, 40);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9\r\nDEF";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 200);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(52, 40);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "4\r\nGHI";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(71, 200);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(52, 40);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "5\r\nJKL";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(129, 200);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(52, 40);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "6\r\nMNO";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 40);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "1\r\nPQRS";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(70, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 40);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "2\r\nTUV";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(129, 246);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(52, 40);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "3\r\nWXYZ";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // symbtn
+            // 
+            this.symbtn.Location = new System.Drawing.Point(12, 292);
+            this.symbtn.Name = "symbtn";
+            this.symbtn.Size = new System.Drawing.Size(52, 40);
+            this.symbtn.TabIndex = 16;
+            this.symbtn.Text = "*-_";
+            this.symbtn.UseVisualStyleBackColor = true;
+            // 
+            // triplebtn
+            // 
+            this.triplebtn.Location = new System.Drawing.Point(70, 292);
+            this.triplebtn.Name = "triplebtn";
+            this.triplebtn.Size = new System.Drawing.Size(52, 40);
+            this.triplebtn.TabIndex = 17;
+            this.triplebtn.Text = "000\r\n[ _ ]";
+            this.triplebtn.UseVisualStyleBackColor = true;
+            // 
+            // singlebtn
+            // 
+            this.singlebtn.Location = new System.Drawing.Point(128, 292);
+            this.singlebtn.Name = "singlebtn";
+            this.singlebtn.Size = new System.Drawing.Size(52, 40);
+            this.singlebtn.TabIndex = 18;
+            this.singlebtn.Text = "#\r\n0 00";
+            this.singlebtn.UseVisualStyleBackColor = true;
+            // 
+            // entbtn
+            // 
+            this.entbtn.Location = new System.Drawing.Point(195, 154);
+            this.entbtn.Name = "entbtn";
+            this.entbtn.Size = new System.Drawing.Size(60, 178);
+            this.entbtn.TabIndex = 19;
+            this.entbtn.Text = "Enter";
+            this.entbtn.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 349);
+            this.Controls.Add(this.entbtn);
+            this.Controls.Add(this.singlebtn);
+            this.Controls.Add(this.triplebtn);
+            this.Controls.Add(this.symbtn);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.sendbtn);
+            this.Controls.Add(this.prevbtn);
+            this.Controls.Add(this.nextbtn);
+            this.Controls.Add(this.modebtn);
+            this.Controls.Add(this.settingbtn);
+            this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Mini Keyboard";
             this.ResumeLayout(false);
 
         }
@@ -54,6 +262,25 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button settingbtn;
+        private System.Windows.Forms.Button modebtn;
+        private System.Windows.Forms.Button nextbtn;
+        private System.Windows.Forms.Button prevbtn;
+        private System.Windows.Forms.Button sendbtn;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button symbtn;
+        private System.Windows.Forms.Button triplebtn;
+        private System.Windows.Forms.Button singlebtn;
+        private System.Windows.Forms.Button entbtn;
     }
 }
 
